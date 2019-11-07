4cff34d496ce1a07e8cef45881b48397bbad9f8f
Gaiokane <qk632773809@vip.qq.com>
2019-11-07 22:28:14 +0800

    主窗体调用用户控件功能，不支持直接赋值给用户控件中，程序启动时，先加载用户控件

--- UserControlDemo/Form1.cs
+++ UserControlDemo/Form1.cs
@@ -16,5 +16,29 @@ namespace UserControlDemo
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            userControl11.Txt_txtbox = "11";
+            userControl11.Txt_cmbox = new string[] { "q1", "q2", "q3" };
+            userControl11.Txt_btnname = "12";
+        }
+
+        private void userControl11_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            userControl11.Txt_txtbox = "21";
+            userControl11.Txt_cmbox = new string[] { "w1", "w2", "w3" };
+            userControl11.Txt_btnname = "22";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            userControl11.showlabel();
+        }
     }
 }
--- UserControlDemo/UserControl1.cs
+++ UserControlDemo/UserControl1.cs
@@ -16,5 +16,41 @@ namespace UserControlDemo
         {
             InitializeComponent();
         }
+
+        private string txt_txtbox;
+        private string[] txt_cmbox = null;
+        private string txt_btnname;
+        private string txt_cmboxselect;
+
+        public string Txt_txtbox { get => txt_txtbox; set => txt_txtbox = value; }
+        public string[] Txt_cmbox { get => txt_cmbox; set => txt_cmbox = value; }
+        public string Txt_btnname { get => txt_btnname; set => txt_btnname = value; }
+        public string Txt_cmboxselect { get => txt_cmboxselect; set => txt_cmboxselect = value; }
+
+        private void UserControl1_Load(object sender, EventArgs e)
+        {
+            /*
+            textBox1.Text = txt_txtbox;
+
+            for (int i = 0; i < txt_cmbox.Length; i++)
+            {
+                comboBox1.Items.Add(txt_cmbox[i]);
+            }
+
+            button1.Text = txt_btnname;
+            */
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            showlabel();
+        }
+
+        public void showlabel()
+        {
+            Txt_txtbox = textBox1.Text;
+            txt_cmboxselect = comboBox1.SelectedText;
+            label1.Text = Txt_txtbox + " " + txt_cmboxselect;
+        }
     }
 }
