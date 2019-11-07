9cdb3692ba96a00054fae6acc115b4a24bd3e268
Gaiokane <qk632773809@vip.qq.com>
2019-11-07 21:56:22 +0800

    用户控件Demo项目创建

--- UserControlDemo/UserControl1.Designer.cs
+++ UserControlDemo/UserControl1.Designer.cs
@@ -1,13 +1,13 @@
 ﻿namespace UserControlDemo
 {
-    partial class Form1
+    partial class UserControl1
     {
-        /// <summary>
+        /// <summary> 
         /// 必需的设计器变量。
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
+        /// <summary> 
         /// 清理所有正在使用的资源。
         /// </summary>
         /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
@@ -20,9 +20,9 @@
             base.Dispose(disposing);
         }
 
-        #region Windows 窗体设计器生成的代码
+        #region 组件设计器生成的代码
 
-        /// <summary>
+        /// <summary> 
         /// 设计器支持所需的方法 - 不要修改
         /// 使用代码编辑器修改此方法的内容。
         /// </summary>
@@ -30,14 +30,12 @@
         {
             this.SuspendLayout();
             // 
-            // Form1
+            // UserControl1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Form1";
+            this.Name = "UserControl1";
+            this.Size = new System.Drawing.Size(298, 214);
             this.ResumeLayout(false);
 
         }
@@ -45,4 +43,3 @@
         #endregion
     }
 }
-
